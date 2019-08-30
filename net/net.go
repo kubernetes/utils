cbc8ded2893bf672ac4dd2373346c23f30de77bf
Yassine TIJANI <ytijani@vmware.com>
2019-08-30 22:25:08 +0100

    adding functions for ipallocator
    Signed-off-by: Yassine TIJANI <ytijani@vmware.com>

--- net/net.go
+++ net/net.go
@@ -18,6 +18,7 @@ package net
 
 import (
 	"fmt"
+	"math/big"
 	"net"
 	"strconv"
 )
@@ -145,4 +146,37 @@ func ParsePort(port string) (int, error) {
 		return 0, fmt.Errorf("%d is not a valid port: must be between 1 and 65535, inclusive", portInt)
 	}
 	return portInt, nil
+}
+
+// BigForIP creates a big.Int based on the provided net.IP
+func BigForIP(ip net.IP) *big.Int {
+	b := ip.To4()
+	if b == nil {
+		b = ip.To16()
+	}
+	return big.NewInt(0).SetBytes(b)
+}
+
+// AddIPOffset adds the provided integer offset to a base big.Int representing a
+// net.IP
+func AddIPOffset(base *big.Int, offset int) net.IP {
+	return net.IP(big.NewInt(0).Add(base, big.NewInt(int64(offset))).Bytes())
+}
+
+// RangeSize returns the size of a range in valid addresses.
+func RangeSize(subnet *net.IPNet) int64 {
+	ones, bits := subnet.Mask.Size()
+	if bits == 32 && (bits-ones) >= 31 || bits == 128 && (bits-ones) >= 127 {
+		return 0
+	}
+	return int64(1) << uint(bits-ones)
+}
+
+// GetIndexedIP returns a net.IP that is subnet.IP + index in the contiguous IP space.
+func GetIndexedIP(subnet *net.IPNet, index int) (net.IP, error) {
+	ip := AddIPOffset(BigForIP(subnet.IP), index)
+	if !subnet.Contains(ip) {
+		return nil, fmt.Errorf("can't generate IP with index %d from subnet. subnet too small. subnet: %q", index, subnet)
+	}
+	return ip, nil
 }

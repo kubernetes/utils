953deb3c8027f37f84a17618a8cc84049830c216
Manu Gupta <mgpta@amazon.com>
2021-09-15 12:48:45 -0700

    fix: read small files or files that show zero size once as a large file.
    /proc/mounts zero as disk usage size
    
    ```
    ubuntu@ip-172-31-49-2:~/go/src/k8s.io/kubernetes/vendor/k8s.io/utils/io$
    du -h /proc/mounts
    0	/proc/mounts
    ubuntu@ip-172-31-49-2:~/go/src/k8s.io/kubernetes/vendor/k8s.io/utils/io$
    ```
    
    As a result of which between multiple read calls, the content of
    /proc/mounts might change, this change introduceces reading 0 or small
    files to read the entire file in 1 go.

--- io/read.go
+++ io/read.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"os"
 )
 
 // ErrLimitReached means that the read limit is reached.
@@ -41,6 +42,23 @@ func ConsistentRead(filename string, attempts int) ([]byte, error) {
 // introduces a sync callback that can be used by the tests to mutate the file
 // from which the test data is being read
 func consistentReadSync(filename string, attempts int, sync func(int)) ([]byte, error) {
+
+	// get the size of the file.
+	size, err := getFileSize(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	// if the size of the file is less than 512 bytes, assume that reading from
+	// /proc or /sys like cases for Linux.
+	if size < 512 {
+		if contents, err := consistentReadFileNoStat(filename); err == nil {
+			return contents, nil
+		}
+		// fall back to consistent read because this is now a utility and an error here can break
+		// expected behavior.
+	}
+
 	oldContent, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -60,6 +78,52 @@ func consistentReadSync(filename string, attempts int, sync func(int)) ([]byte, 
 		oldContent = newContent
 	}
 	return nil, InconsistentReadError{filename, attempts}
+}
+
+// get filesize returns the filesize
+func getFileSize(filename string) (int, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+
+	var size int
+	// Calling stat should be okay as it supports both Windows and Linux
+	// Ref (unix and posix compliant): https://cs.opensource.google/go/go/+/refs/tags/go1.17.1:src/os/stat_unix.go;l=16
+	// Ref (windows): https://cs.opensource.google/go/go/+/refs/tags/go1.17.1:src/os/stat_windows.go
+	if info, err := f.Stat(); err == nil {
+		size64 := info.Size()
+		if int64(int(size64)) == size64 {
+			size = int(size64)
+		}
+	}
+	size++ // one byte for final read at EOF
+	return size, nil
+}
+
+// consistentReadFileNoStat is inspired from prometheus code base for /proc and /sys
+func consistentReadFileNoStat(filename string) ([]byte, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+
+	const maxBufferSize = 1024 * 512
+	var contents = make([]byte, maxBufferSize)
+
+	// use f.Read as it points to File.Read which will directly call os dependent functions
+	// For Linux: https://cs.opensource.google/go/go/+/master:src/syscall/syscall_unix.go;l=188;bpv=1;bpt=1?q=syscall.Read&ss=go%2Fgo
+	// Also, do not prefer ReadAtMost as it internally results in a loop for read depending on the buffer capacity.
+	// This will result in multiple syscall.Read
+	n, err := f.Read(contents)
+	if err != nil {
+		// not returning contents to ensure that the bytes are not used up the stack as they occupy memory.
+		return []byte{}, err
+	}
+	// slice the byte before returning to the x number of bytes so that the maximum buffer is not reached
+	return contents[:n], nil
 }
 
 // InconsistentReadError is returned from ConsistentRead when it cannot get
